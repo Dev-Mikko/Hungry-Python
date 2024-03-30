2f5397df025419665ba1343102aed0add37878ec
Michele De Palma <depalma_michele@outlook.it>
2024-03-30 13:33:16 +0100

    Added Direction and Point classes in SnakeGame.py

--- src/SnakeGame.py
+++ src/SnakeGame.py
@@ -1,2 +1,12 @@
+from collections import namedtuple
+from enum import Enum
 
+class Direction(Enum):
+    UP = 1
+    LEFT = 2
+    DOWN = 3
+    RIGHT = 4
+
+
+Point = namedtuple("Point", "x, y")
 
